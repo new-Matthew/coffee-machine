1ab63bc6aa75577876dfc0416b5ee339036678a6
matheus <matheusrodrig16@gmail.com>
2025-01-30 21:59:01 -0300

    feat: report resources and money

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from resources import menu
 from resources import resources as res
 
-
+profit = 0
 is_on = True
 while is_on:
     choice = input("What do you like? (espresso/latte/capuccino): ")
     if choice == "off":
         is_on = False
+    elif choice == "report":
+        print(f"water: {res["water"]}ml")
+        print(f"milk: {res["milk"]}ml")
+        print(f"coffee: {res["coffee"]}g")
+        print(f"Money: ${profit}")
     
